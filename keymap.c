a3d426d79d45d08b445c4cbfbd76151618865f4a
Gustav Johansson <gavus@me.com>
2023-02-23 13:02:00 +0100

    Make lsft act as nubs

--- keymap.c
+++ keymap.c
@@ -46,19 +46,21 @@ enum {
 #define RA_NUBS RALT(KC_NUBS)
 #define LS_MINS LSFT(KC_MINUS)
 #define RA_MINS RALT(KC_MINUS)
+#define RA_EQL  RALT(KC_EQUAL)
 
 #define LT_SPC LT(_SPACE, KC_SPC)
 #define LT_F LT(_ADJUST, KC_F)
 #define CTL_ESC LCTL_T(KC_ESC)
+#define SFL_NUB LSFT_T(KC_NUBS)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: plain Qwerty without layer switching
      *         ,---------------. ,---------------. ,---------------.
      *         |F13|F14|F15|F16| |F17|F18|F19|F20| |F21|F22|F23|F24|
-     * ,----.  |---------------| |---------------| |---------------| ,-----------. ,---------------. ,-------.
-     * |Caps|  |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12| |PrS|ScL|Pau| |VDn|VUp|Mut|Pwr| | Help  |
-     * `----'  `---------------' `---------------' `---------------' `-----------' `---------------' `-------'
+     * ,-----. |---------------| |---------------| |---------------| ,-----------. ,---------------. ,-------.
+     * |CtEsc| |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12| |PrS|ScL|Pau| |VDn|VUp|Mut|Pwr| | Help  |
+     * `-----' `---------------' `---------------' `---------------' `-----------' `---------------' `-------'
      * ,-----------------------------------------------------------. ,-----------. ,---------------. ,-------.
      * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|JPY|Bsp| |Ins|Hom|PgU| |NmL|  /|  *|  -| |Stp|Agn|
      * |-----------------------------------------------------------| |-----------| |---------------| |-------|
@@ -66,18 +68,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------| `-----------' |---------------| |-------|
      * |CtlEsc|  A|  S|  D| Fn|  G|  H|  J|  K|  L|  ;|  :|  #|Retn|               |  4|  5|  6|KP,| |Sel|Cpy|
      * |-----------------------------------------------------------|     ,---.     |---------------| |-------|
-     * |Shft|  <|  Z|  X|  C|  V|  B|  N|  M|  ,|  ,|  /| RO|Shift |     |Up |     |  1|  2|  3|KP=| |Exe|Pst|
+     * |Shi<|  <|  Z|  X|  C|  V|  B|  N|  M|  ,|  ,|  /| RO|Shift |     |Up |     |  1|  2|  3|KP=| |Exe|Pst|
      * |-----------------------------------------------------------| ,-----------. |---------------| |-------|
      * |Ctl|Gui|Alt|MHEN|HNJ| SpacFN |H/E|HENK|KANA|Alt|Gui|App|Ctl| |Lef|Dow|Rig| |  0    |  .|Ent| |Fnd|Cut|
      * `-----------------------------------------------------------' `-----------' `---------------' `-------'
      */
     [_QWERTY] = LAYOUT_all(
                       KC_F13,  KC_F14,  KC_F15,  KC_F16, KC_F17, KC_F18, KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,
-    KC_CAPS,          KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,               KC_PSCR, KC_SCRL, KC_PAUS,    KC_VOLD, KC_VOLU, KC_MUTE, KC_PWR,     KC_HELP,
+    CTL_ESC,          KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,               KC_PSCR, KC_SCRL, KC_PAUS,    KC_VOLD, KC_VOLU, KC_MUTE, KC_PWR,     KC_HELP,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_INT3, KC_BSPC,     KC_INS,  KC_HOME, KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,    KC_STOP, KC_AGIN,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,     KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,    KC_MENU, KC_UNDO,
     CTL_ESC, KC_A,    KC_S,    KC_D,    LT_F,    KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_NUHS, KC_ENT,                                    KC_P4,   KC_P5,   KC_P6,   KC_PCMM,    KC_SLCT, KC_COPY,
-    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_INT1, KC_RSFT,              KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PEQL,    KC_EXEC, KC_PSTE,
+    SFL_NUB, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_INT1, KC_RSFT,              KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PEQL,    KC_EXEC, KC_PSTE,
     KC_LCTL, KC_LGUI, KC_LALT, KC_INT5, KC_LNG2,         LT_SPC,         KC_LNG1, KC_INT4, KC_INT2, KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,     KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT, KC_PENT,    KC_FIND, KC_CUT
     ),
     [_SPACE] = LAYOUT_all(
@@ -86,8 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ______,  ______,  ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______, ______,     ______,______,______,    ______,______,______,______,    ______,______,
     ______,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,    KC_7,    KC_8,   KC_9,    KC_0,   KC_MINS,______,         ______,     ______,______,______,    ______,______,______,______,    ______,______,
     ______,  LS_1,    LS_2,    LS_3,    LS_4,   LS_5,   LS_6,    LS_7,    LS_8,   LS_9,    LS_0,   LS_MINS,        ______, ______,                              ______,______,______,______,    ______,______,
-    ______,  RA_NUBS, RA_1,    RA_2,    RA_3,   RA_4,   RA_5,    RA_6,    RA_7,   RA_8,    RA_9,   RA_0,           RA_MINS,______,            ______,           ______,______,______,______,    ______,______,
-    KC_LGUI, _______, ______,  ______,  ______,         ______,           ______, ______,  ______, ______, ______, ______, ______,     ______,______,______,    ______,       ______,______,    ______,______
+    ______,  RA_NUBS, RA_1,    RA_2,    RA_3,   RA_4,   RA_5,    RA_6,    RA_7,   RA_8,    RA_9,   RA_0,           RA_MINS,RA_EQL,            ______,           ______,______,______,______,    ______,______, KC_LGUI, _______, ______,  ______,  ______,         ______,           ______, ______,  ______, ______, ______, ______, ______,     ______,______,______,    ______,       ______,______,    ______,______
     ),
     [_ADJUST] = LAYOUT_all(
                       ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______,
